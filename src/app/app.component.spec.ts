6f936459921762d01e9d48f5df83796787e300e9
NdekoCode <arickbulakali@gmail.com>
2023-06-17 11:44:03 +0200

    add services test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -25,11 +21,8 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-testing');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-testing app is running!');
+  it('Should test a equality', () => {
+    const add = 4 + 4;
+    expect(add).toBe(8);
   });
 });
--- src/shared/services/todos.service.spec.ts
+++ src/shared/services/todos.service.spec.ts
@@ -3,14 +3,36 @@ import { TestBed } from '@angular/core/testing';
 import { TodosService } from './todos.service';
 
 describe('TodosService', () => {
-  let service: TodosService;
+  let _todosService: TodosService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TodosService);
+    _todosService = TestBed.inject(TodosService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(_todosService).toBeTruthy();
+  });
+  it('Should get all todo', () => {
+    const todos = _todosService.todos;
+    expect(todos).toBeDefined();
+    expect(todos.length).toBeGreaterThan(1);
+    expect(Array.isArray(todos)).toBe(true);
+  });
+  it('Should add Todo', () => {
+    const count = _todosService.todos.length;
+    const content = 'Apprendre le test';
+    _todosService.addTodo(content);
+    expect(_todosService.todos.length).toBe(count + 1);
+    expect(
+      _todosService.todos.find((t) => t.content === content)?.content
+    ).toBe(content);
+  });
+  it('Should delete a tood', () => {
+    const count = _todosService.todos.length;
+    const id = _todosService.todos[0].id;
+    _todosService.removeTodo(id);
+    expect(_todosService.todos.length).toBe(count - 1);
+    expect(_todosService.todos.find((t) => t.id == id)).toBeUndefined();
   });
 });
